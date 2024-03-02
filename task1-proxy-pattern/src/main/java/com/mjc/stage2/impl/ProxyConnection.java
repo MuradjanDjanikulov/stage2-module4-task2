daae8ae3d176866c35ad3a4ed55af20bbc5870f2
murod <murad.djanikulov@gmail.com>
2024-03-02 22:09:29 +0500

    proxy connection done

--- task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
+++ task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
@@ -1,15 +1,32 @@
 package com.mjc.stage2.impl;
 
 
-public class ProxyConnection {
+import com.mjc.stage2.Connection;
+
+public class ProxyConnection implements Connection {
     private RealConnection realConnection;
 
     public ProxyConnection(RealConnection realConnection) {
         this.realConnection = realConnection;
     }
 
-    public void reallyClose() {
-        // Write your code here!
+    @Override
+    public void close() {
+        // Return the connection to the ConnectionPool
+        ConnectionPool.getInstance().releaseConnection(this);
     }
-    // Implement methods here!
+
+    public void reallyClose() {
+        // Close the real connection
+        if (realConnection != null && !realConnection.isClosed()) {
+            realConnection.close();
+        }
+    }
+
+    // For example, you might delegate methods like isClosed() to the real connection.
+    @Override
+    public boolean isClosed() {
+        return realConnection == null || realConnection.isClosed();
+    }
+
 }
